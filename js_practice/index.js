efead46e675df8e9540b2219bcd3d4f7a03b9493
Aakshita Agarwal <aakshita0104@gmail.com>
2023-02-20 18:34:33 +0530

    js_intro

---
+++ js_practice/index.js
@@ -0,0 +1,17 @@
+console.log('It works 👌')
+
+updateColor = function(){
+    console.log('hakuna matata is a great song')
+}
+
+function doSomething(){
+    alert('I am doing Something')
+}
+
+animate_button = () => {
+    console.log('I am animating the button')
+}
+
+changeMessage = (e) => {
+    e.target.innerText = 'Button Clicked'
+}
